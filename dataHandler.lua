9ee413a9abab23499d1225d8c06dbc3850b1ff92
Diject <alexkozlovv@gmail.com>
2024-10-30 22:09:20 +0300

    Added new data

--- dataHandler.lua
+++ dataHandler.lua
@@ -8,6 +8,8 @@ this.quests = {}
 this.questByText = {}
 ---@type table<string, questDataGenerator.objectInfo>
 this.questObjects = {}
+---@type questDataGenerator.localVariableByQuestId
+this.localVariablesByQuestId = {}
 
 local isReady = false
 
@@ -17,13 +19,15 @@ function this.init()
     this.quests = json.loadfile("mods\\diject\\quest_guider\\Data\\quests")
     this.questByText = json.loadfile("mods\\diject\\quest_guider\\Data\\questByTopicText")
     this.questObjects = json.loadfile("mods\\diject\\quest_guider\\Data\\questObjects")
+    this.localVariablesByQuestId = json.loadfile("mods\\diject\\quest_guider\\Data\\localVariables")
 
-    if this.quests and this.questObjects and this.questByText then
+    if this.quests and this.questObjects and this.questByText and this.localVariablesByQuestId then
         isReady = true
     else
         this.quests = this.quests or {}
         this.questObjects = this.questObjects or {}
         this.questByText = this.questByText or {}
+        this.localVariablesByQuestId = this.localVariablesByQuestId or {}
     end
 
     return isReady
@@ -32,6 +36,13 @@ end
 ---@return boolean
 function this.isReady()
     return isReady
+end
+
+function this.reset()
+    this.quests = {}
+    this.questObjects = {}
+    this.questByText = {}
+    this.localVariablesByQuestId = {}
 end
 
 return this
