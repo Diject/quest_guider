beb8df4d7cf41b945729e969c17f4db48f5ecedb
Diject <alexkozlovv@gmail.com>
2025-01-09 21:39:28 +0300

    added encoding parameter

--- initializer.lua
+++ initializer.lua
@@ -11,10 +11,18 @@ function this.runDataGeneration(async)
     local maxPos = config.data.data.maxPos
     local dir = tes3.installDirectory.."\\Data Files\\MWSE\\lib\\quest_guider"
     local outputDir = tes3.installDirectory.."\\Data Files\\MWSE\\mods\\diject\\quest_guider\\Data"
+    local encoding
+    if tes3.getLanguageCode() == tes3.languageCode.pol then
+        encoding = "1250"
+    elseif tes3.getLanguageCode() == tes3.languageCode.rus then
+        encoding = "1251"
+    else
+        encoding = "1252"
+    end
 
     -- by morrowind.ini
-    local command = string.format("start /B \"\" /D \"%s\" \"Quest Data Builder.exe\" -p %d -d \"%s\" -o \"%s\" -l %d",
-        dir, maxPos, tes3.installDirectory, outputDir, this.logLevel)
+    local command = string.format("start /B \"\" /D \"%s\" \"Quest Data Builder.exe\" -p %d -d \"%s\" -o \"%s\" -e %s -l %d",
+        dir, maxPos, tes3.installDirectory, outputDir, encoding, this.logLevel)
 
     -- by mod names
     -- local command = string.format("start /B \"\" /D \"%s\" \"Quest Data Builder.exe\" -d \"%s\" -o \"%s\" -l %d -f", dir, tes3.installDirectory, outputDir, this.logLevel)
