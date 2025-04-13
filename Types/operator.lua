63ece8f2015a989a630dea438dae2a29c281faa4
Diject <alexkozlovv@gmail.com>
2025-04-13 13:28:53 +0300

    added invert func

--- Types/operator.lua
+++ Types/operator.lua
@@ -31,4 +31,17 @@ function this.check(a, b, operator)
         (operator == 53 and a <= b)
 end
 
+
+---@param operator integer
+---@return integer
+function this.invert(operator)
+    return (operator == 48 and 49) or
+        (operator == 49 and 48) or
+        (operator == 50 and 53) or
+        (operator == 51 and 52) or
+        (operator == 52 and 51) or
+        (operator == 53 and 50) or 48
+end
+
+
 return this
